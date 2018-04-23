7e7be69ae2bc45ecf943877bd2f1dd64da799de3
naoki <n.globe.us@gmail.com>
2018-04-23 17:31:36 +0900

    fix conding style to standard

--- tests/test-tw-user-link.php
+++ tests/test-tw-user-link.php
@@ -6,56 +6,55 @@
  */
 
 /**
- * User test case.
+ * User est case.
  */
 class TwUserLinkTest extends WP_UnitTestCase {
-
 	/**
-	 * user name in string test.
+	 * User name in string test.
 	 */
 	function test_tw_name_in_string() {
 		$replace_to_link = replace_tw_user_name_to_link('こんにちは、@WordPressさん。');
-		$this->assertEquals('こんにちは、<a href="https://twitter.com/@WordPress">@WordPress</a>さん。',$replace_to_link);
-	}
+		$this->assertEquals('こんにちは、<a href="https://twitter.com/@WordPress">@WordPress</a>さん。', $replace_to_link);
+	 }
 
 	/**
-	 * multiple user name in string test.
+	 * Multiple user name in string test.
 	 */
 	function test_multiple_tw_name_in_string() {
 		$replace_to_link = replace_tw_user_name_to_link('こんにちは、@WordPressさん。僕は、@wordpressdotcomですよ。@Automatticさんに作られています。');
-		$this->assertEquals('こんにちは、<a href="https://twitter.com/@WordPress">@WordPress</a>さん。僕は、<a href="https://twitter.com/@wordpressdotcom">@wordpressdotcom</a>ですよ。<a href="https://twitter.com/@Automattic">@Automattic</a>さんに作られています。',$replace_to_link);
-	}
+		$this->assertEquals('こんにちは、<a href="https://twitter.com/@WordPress">@WordPress</a>さん。僕は、<a href="https://twitter.com/@wordpressdotcom">@wordpressdotcom</a>ですよ。<a href="https://twitter.com/@Automattic">@Automattic</a>さんに作られています。', $replace_to_link);
+	 }
 
 	/**
-	 * prohibited character test.
+	 * Prohibited character test.
 	 */
 	function test_prohibited_character_in_tw_name() {
-		$replace_to_link = replace_tw_user_name_to_link('こんにちは、@ワードプレスさん。僕は、@!"#$%&()=-~^です。彼女は@¥|@`[{;+:*]},<。@.>/?');
-		$this->assertEquals('こんにちは、@ワードプレスさん。僕は、@!"#$%&()=-~^です。彼女は@¥|@`[{;+:*]},<。@.>/?',$replace_to_link);
-	}
+		$replace_to_link = replace_tw_user_name_to_link('こんにちは、@ワードプレスさん。僕は、@!"#$%&()=-~^です。彼女は@¥|@`[{;+:*]}, <。@.>/?');
+		$this->assertEquals('こんにちは、@ワードプレスさん。僕は、@!"#$%&()=-~^です。彼女は@¥|@`[{;+:*]}, <。@.>/?', $replace_to_link);
+	 }
 
 	/**
-	 * available character test.
+	 * Available character test.
 	 */
 	function test_available_character_in_tw_name() {
 		$replace_to_link = replace_tw_user_name_to_link('こんにちは、@ABCDEFGHIJKLMNOさん、@PQRSTUVWXYZ0123さん、@456789_さん、@abcdefghijklmnoさん、@pqrstuvwxyzさん。');
-		$this->assertEquals('こんにちは、<a href="https://twitter.com/@ABCDEFGHIJKLMNO">@ABCDEFGHIJKLMNO</a>さん、<a href="https://twitter.com/@PQRSTUVWXYZ0123">@PQRSTUVWXYZ0123</a>さん、<a href="https://twitter.com/@456789_">@456789_</a>さん、<a href="https://twitter.com/@abcdefghijklmno">@abcdefghijklmno</a>さん、<a href="https://twitter.com/@pqrstuvwxyz">@pqrstuvwxyz</a>さん。',$replace_to_link);
-	}
+		$this->assertEquals('こんにちは、<a href="https://twitter.com/@ABCDEFGHIJKLMNO">@ABCDEFGHIJKLMNO</a>さん、<a href="https://twitter.com/@PQRSTUVWXYZ0123">@PQRSTUVWXYZ0123</a>さん、<a href="https://twitter.com/@456789_">@456789_</a>さん、<a href="https://twitter.com/@abcdefghijklmno">@abcdefghijklmno</a>さん、<a href="https://twitter.com/@pqrstuvwxyz">@pqrstuvwxyz</a>さん。', $replace_to_link);
+	 }
 
 	/**
-	 * <br> tag in string test.
+	 * A <br> tag in string test.
 	 */
 	function test_br_tag_in_string() {
 		$replace_to_link = replace_tw_user_name_to_link('こんにちは、@WordPressさん。<br>元気ですか？');
-		$this->assertEquals('こんにちは、<a href="https://twitter.com/@WordPress">@WordPress</a>さん。<br>元気ですか？',$replace_to_link);
-	}
+		$this->assertEquals('こんにちは、<a href="https://twitter.com/@WordPress">@WordPress</a>さん。<br>元気ですか？', $replace_to_link);
+	 }
 
 	/**
-	 * list user name test.
+	 * List user name test.
 	 */
 	function test_list_tw_name() {
 		$replace_to_link = replace_tw_user_name_to_link('こちらはWordPress関連のアカウント、<ul><li>@WordPressさん。</li><li>@wordpressdotcom</li><li>@Automattic</li></ul>です。');
-		$this->assertEquals('こちらはWordPress関連のアカウント、<ul><li><a href="https://twitter.com/@WordPress">@WordPress</a>さん。</li><li><a href="https://twitter.com/@wordpressdotcom">@wordpressdotcom</a></li><li><a href="https://twitter.com/@Automattic">@Automattic</a></li></ul>です。',$replace_to_link);
-	}
+		$this->assertEquals('こちらはWordPress関連のアカウント、<ul><li><a href="https://twitter.com/@WordPress">@WordPress</a>さん。</li><li><a href="https://twitter.com/@wordpressdotcom">@wordpressdotcom</a></li><li><a href="https://twitter.com/@Automattic">@Automattic</a></li></ul>です。', $replace_to_link);
+	 }
 
-}
+ }
--- tw-user-link.php
+++ tw-user-link.php
@@ -11,17 +11,18 @@
  *
  * @package         Tw_User_Link
  */
+
 add_filter('the_content', 'replace_tw_user_name_to_link');
 
-function replace_tw_user_name_to_link($content){
-
-    if(preg_match_all("/@[0-9a-z_]{1,15}/i",$content, $search)){
-        $user = $search[0];
-        for($i = 0, $size = count($user); $i < $size; ++$i) {
-            $content = str_replace("$user[$i]", '<a href="https://twitter.com/'.esc_attr($user[$i]).'">'.esc_attr($user[$i]).'</a>', $content);
-        }
-    }
-
-    return $content;
-
-}
+/**
+ * Search twitter user name and replace it to it's link.
+ */
+function replace_tw_user_name_to_link( $content ) {
+	if (preg_match_all('/@[0-9a-z_]{1,15}/i', $content, $search)) {
+		$user = $search[ 0 ];
+		for ($i = 0, $size = count($user); $i < $size; ++$i) {
+			$content = str_replace("$user[$i]", '<a href="https://twitter.com/' . esc_attr($user[ $i ]) . '">' . esc_attr($user[ $i ]) . '</a>', $content);
+         }
+     }
+     return $content;
+ }
