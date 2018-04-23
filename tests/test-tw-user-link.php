1a6b98ef76dacc5c57480b9fa3901d93a2b9606b
naoki <n.globe.us@gmail.com>
2018-04-23 16:00:19 +0900

    add tests

--- tests/test-tw-user-link.php
+++ tests/test-tw-user-link.php
@@ -13,8 +13,23 @@ class TwUserLinkTest extends WP_UnitTestCase{
     }
 
     function test_prohibited_character_in_tw_name(){
-        $replace_to_link = replace_tw_user_name_to_link('こんにちは、@ワードプレスさん。僕は、@"#$%&^-|?+です。');
-        $this->assertEquals('こんにちは、@ワードプレスさん。僕は、@"#$%&^-|?+です。',$replace_to_link);
+        $replace_to_link = replace_tw_user_name_to_link('こんにちは、@ワードプレスさん。僕は、@!"#$%&()=-~^です。彼女は@¥|@`[{;+:*]},<。@.>/?');
+        $this->assertEquals('こんにちは、@ワードプレスさん。僕は、@!"#$%&()=-~^です。彼女は@¥|@`[{;+:*]},<。@.>/?',$replace_to_link);
     }
+
+	function test_available_character_in_tw_name(){
+		$replace_to_link = replace_tw_user_name_to_link('こんにちは、@ABCDEFGHIJKLMNOさん、@PQRSTUVWXYZ0123さん、@456789_さん、@abcdefghijklmnoさん、@pqrstuvwxyzさん。');
+		$this->assertEquals('こんにちは、<a href="https://twitter.com/@ABCDEFGHIJKLMNO">@ABCDEFGHIJKLMNO</a>さん、<a href="https://twitter.com/@PQRSTUVWXYZ0123">@PQRSTUVWXYZ0123</a>さん、<a href="https://twitter.com/@456789_">@456789_</a>さん、<a href="https://twitter.com/@abcdefghijklmno">@abcdefghijklmno</a>さん、<a href="https://twitter.com/@pqrstuvwxyz">@pqrstuvwxyz</a>さん。',$replace_to_link);
+	}
+
+	function test_br_tag_in_string(){
+		$replace_to_link = replace_tw_user_name_to_link('こんにちは、@WordPressさん。<br>元気ですか？');
+		$this->assertEquals('こんにちは、<a href="https://twitter.com/@WordPress">@WordPress</a>さん。<br>元気ですか？',$replace_to_link);
+	}
+
+	function test_list_tw_name(){
+		$replace_to_link = replace_tw_user_name_to_link('こちらはWordPress関連のアカウント、<ul><li>@WordPressさん。</li><li>@wordpressdotcom</li><li>@Automattic</li></ul>です。');
+		$this->assertEquals('こちらはWordPress関連のアカウント、<ul><li><a href="https://twitter.com/@WordPress">@WordPress</a>さん。</li><li><a href="https://twitter.com/@wordpressdotcom">@wordpressdotcom</a></li><li><a href="https://twitter.com/@Automattic">@Automattic</a></li></ul>です。',$replace_to_link);
+	}
 
 }
