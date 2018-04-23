92c4a4753021fa0bae4430efa8ce37f2201da354
naoki <n.globe.us@gmail.com>
2018-04-24 07:08:04 +0900

    fix conding style to standard

--- tw-user-link.php
+++ tw-user-link.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     Tw User Link
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://github.com/naogify/tw-user-link
+ * Description:     Search twitter user name and replace it to it's link from article.
+ * Author:          Naoki Ohashi
+ * Author URI:      https://naoki-is.me/
  * Text Domain:     tw-user-link
  * Domain Path:     /languages
  * Version:         0.1.0
@@ -13,13 +13,14 @@
  */
 
 /**
- * Search twitter user name and replace it to it's link.
+ * Function search twitter username and make link for it.
  *
- * @param type $var It's varible for "the_content".
+ * @param string $content the_content.
  *
- * @return mixed
+ * @return string
  */
 function replace_tw_user_name_to_link( $content ) {
+	var_dump($content);
 	if ( preg_match_all( '/@[0-9a-z_]{1,15}/i', $content, $search ) ) {
 		$user = $search[0];
 		for ( $i = 0, $size = count( $user ); $i < $size; ++ $i ) {
