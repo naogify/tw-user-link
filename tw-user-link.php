11922f0e49540787f205baa699885a2bd82d6b4a
naoki <n.globe.us@gmail.com>
2018-04-24 13:43:16 +0900

    for => foreach

--- tw-user-link.php
+++ tw-user-link.php
@@ -23,11 +23,9 @@ add_filter( 'the_content', 'replace_tw_user_name_to_link' );
 function replace_tw_user_name_to_link( $content ) {
 	if ( preg_match_all( '/@[0-9a-z_]{1,15}/i', $content, $search ) ) {
 		$user = $search[0];
-		$size = count( $user );
-		for ( $i = 0; $i < $size; ++ $i ) {
-			$content = str_replace( "$user[$i]", '<a href="https://twitter.com/' . esc_attr( $user[ $i ] ) . '">' . esc_attr( $user[ $i ] ) . '</a>', $content );
+		foreach ( $user as $value ){
+			$content = str_replace( "$value", '<a href="https://twitter.com/' . esc_attr( $value ) . '">' . esc_attr( $value ) . '</a>', $content );
 		}
 	}
-
 	return $content;
 }
